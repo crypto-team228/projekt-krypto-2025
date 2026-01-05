8038d2fab527762d6071d77a700af5d2599af69c
oleksandrabelmas0908 <oleksandr.abelmas0908@gmail.com>
2026-01-05 10:54:15 +0100

    fix modes

--- include/Tools/enums.h
+++ include/Tools/enums.h
@@ -12,10 +12,9 @@ enum StringFormat{
 
 // Note: Mode enum is now defined in algorithm.hpp
 // This is kept for reference only - use algorithm.hpp version
-enum ModeReference {
+enum Mode {
 	ECB,   // Electronic Codebook
 	CBC,   // Cipher Block Chaining
-	CFB,   // Cipher Feedback (not yet implemented)
 	CTR,   // Counter
 	GCM    // Galois/Counter Mode (uses GHASH internally)
 };
